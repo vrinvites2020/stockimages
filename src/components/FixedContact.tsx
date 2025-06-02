2903976ddbc8d0ada1e4a1c0b178ff96eab06f66
vignesh3022 <130534872+vignesh3022@users.noreply.github.com>
2025-06-02 13:37:28 +0530

    telegram and whatsapp details

--- src/components/FixedContact.tsx
+++ src/components/FixedContact.tsx
@@ -8,11 +8,11 @@ const FixedContact = () => {
          <div className="fixed bottom-4 right-4 z-50 flex flex-col items-center p-4 rounded-lg shadow-lg bg-gray-800 bg-opacity-75 md:bottom-8 md:right-8">
          <p className="text-white text-sm font-medium drop-shadow mb-2">For more details contact</p>
          <div className="flex space-x-4">
-           <a href="https://wa.me/+918500003951" target="_blank" rel="noopener noreferrer" className="transition-transform transform hover:scale-110">
+           <a href="https://whatsapp.com/channel/0029Vb5pUGIEwEjtvpcKZD0U" target="_blank" rel="noopener noreferrer" className="transition-transform transform hover:scale-110">
              {/* Font Awesome WhatsApp icon */}
              <FontAwesomeIcon icon={faWhatsapp} className="text-green-500 text-4xl md:text-5xl" />
            </a>
-           <a href="https://t.me/Vinaykumar7093" target="_blank" rel="noopener noreferrer" className="transition-transform transform hover:scale-110">
+           <a href="https://t.me/+vh3UIWbhB09kYjU1" target="_blank" rel="noopener noreferrer" className="transition-transform transform hover:scale-110">
              {/* Font Awesome Telegram icon */}
              <FontAwesomeIcon icon={faTelegram} className="text-blue-500 text-4xl md:text-5xl" />
            </a>
